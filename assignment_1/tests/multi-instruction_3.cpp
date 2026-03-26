d534fa264055398bbe7edea9f29c1b72506fa754
Ari-LD <arild.kuti7902@gmail.com>
2026-03-26 19:45:23 +0100

    added multi and recursive logic, doxygen comments, the new AND might have a slight problem, missing comments for isRequired and static Fn firstOf()

--- assignment_1/tests/multi-instruction_3.cpp
+++ assignment_1/tests/multi-instruction_3.cpp
@@ -9,9 +9,17 @@ int fun (int x)
     int e = x ^ 5;
     int f = e ^ 5;
 
+    //con solo due istruzioni funziona correttamente
+    //con queste tre per qualche motivo elimina la seconda e fa una AND tra 
+    //la costante dell'ultima e 0 (non dovrebbe)
+    //se al posto di 8 mettiamo una roba tipo 1 allora funziona, quindi boh
+    int g = x & 8;
+    int h = g & 5;
+    int i = h & 2;
+
     int w = x ^ 3;
     int y = w ^ 7;
     int z = y ^ 4;
 
-    return z;
+    return i + z;
 }
