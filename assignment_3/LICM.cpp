cdcf79a2b225d7fcd965ace514758739b2bb2558
Carmine De Rosa <derosacarmine766@gmail.com>
2026-04-15 19:23:04 +0200

    added first implementation of LI analysis

---
+++ assignment_3/LICM.cpp
@@ -0,0 +1,117 @@
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include <llvm-19/llvm/IR/Analysis.h>
+
+#include <llvm-19/llvm/IR/BasicBlock.h>
+#include <llvm-19/llvm/IR/Constant.h>
+#include <llvm-19/llvm/IR/Constants.h>
+#include <llvm-19/llvm/IR/Instruction.h>
+#include <llvm-19/llvm/IR/Value.h>
+#include <llvm-19/llvm/Support/Casting.h>
+#include "llvm/IR/Dominators.h"
+
+#include <set>
+#include <map>
+
+using namespace llvm;
+
+#define DEBUG true
+
+namespace {
+
+// cheat function ?
+// loop->hasLoopInvariantOperands(const Instruction *I)
+
+struct LoopInvariantCodeMotion : PassInfoMixin<LoopInvariantCodeMotion> {
+
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+
+        LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+        auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
+        std::set<Instruction*> invariantSet;
+
+        //Loop Invariant logic
+        for (Loop *LL : LI.getLoopsInPreorder()) {
+            if(!LL->isLoopSimplifyForm()) continue;
+            
+
+            bool changed = true;
+            while (changed) {
+                changed = false;
+                
+                for (BasicBlock *BB : LL->getBlocks()) {
+                    for (Instruction &I : *BB) {
+                        if (I.getOpcode() == Instruction::PHI || invariantSet.count(&I) > 0) 
+                            continue;        
+                        
+                        // TODO: check if there are other instructions that can be used
+                        if(I.getNumOperands() != 2)
+                            continue;
+
+                        bool isInvariant = true;
+                        
+                        for (Use &Op : I.operands()) {
+                            Value *operandValue = Op.get();
+                            
+                            if (auto *op_instr = dyn_cast<Instruction>(operandValue)) {
+
+                                if (LL->contains(op_instr->getParent()) && invariantSet.count(op_instr) == 0) {
+                                    isInvariant = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (isInvariant) {
+                            invariantSet.insert(&I);
+                            changed = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Debug output for loop invariant instructions
+        if(DEBUG){
+            for (auto instr : invariantSet) {
+                instr->print(outs());
+                outs() << "\n";
+            }
+        }
+        
+        //TODO: Code Motion Logic
+
+        return PreservedAnalyses::all();
+    }
+
+
+
+    static bool isRequired() { return true; }
+}; 
+}
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getLoopPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "LoopInvariantCodeMotion", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "LI-CM") {
+                    FPM.addPass(LoopInvariantCodeMotion());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getLoopPassPluginInfo();
+}
---
+++ assignment_3/tests/single_loop.cpp
@@ -0,0 +1,17 @@
+void fun(int n){
+
+    int b=3;
+    int c=4;
+    int e;
+    for(int i=0;i<n;i++){
+        int a = b+c;
+        if(a<=10){
+            e = 2;
+        }
+        else {
+            e = 3;
+        }
+        int d = a+1;
+        int f = e+2;
+    }
+}
