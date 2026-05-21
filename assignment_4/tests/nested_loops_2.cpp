dfdb1a448bb9c0152bbbfd13a81f9a1bf9912b48
Ari-LD <arild.kuti7902@gmail.com>
2026-05-21 14:50:18 +0200

    new test

---
+++ assignment_4/tests/nested_loops_2.cpp
@@ -0,0 +1,13 @@
+int fun(int n, int m) {
+  int a = 0, b = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      a = j + i;
+    }
+    for (int j = 0; j < m; j++) {
+      b = j * i;
+    }
+  }
+
+  return a + b;
+}
