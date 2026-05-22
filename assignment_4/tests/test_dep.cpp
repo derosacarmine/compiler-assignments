15804a04ceec01afc56163491d6bf4612d7f8912
Carmine De Rosa <derosacarmine766@gmail.com>
2026-05-22 15:31:45 +0200

    added test for negative dependencies

--- assignment_4/tests/test_dep.cpp
+++ assignment_4/tests/test_dep.cpp
@@ -1,10 +1,14 @@
-int fun(int n) {
+int fun(int n, int arr[]) {
   int c = 10;
   for (int i = 0; i < n; i++) {
     c = i;
   }
   for (int i = 0; i < n; i++) {
     int d = i + c;
+    arr[i] = d * 2;
+  }
+  for (int i = 0; i < n; i++) {
+    int d = arr[i + 1] + c;
   }
   return 0;
 }
