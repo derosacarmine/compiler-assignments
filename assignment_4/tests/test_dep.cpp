b78cf00660820f715e86f00df9ab7c16671b146e
Carmine De Rosa <derosacarmine766@gmail.com>
2026-06-16 20:09:13 +0200

    added note for the use of long data type in test_dep

--- assignment_4/tests/test_dep.cpp
+++ assignment_4/tests/test_dep.cpp
@@ -8,7 +8,11 @@ int fun(int n, int arr[]) {
     arr[i] = c * 2;
   }
   for (long i = 0; i < n; i++) {
-    int d = arr[i + 2] + 3;
+    int d = arr[i + 2] +
+            3; // if we operate on the variable before it is used for
+               // indexing, it's necessary to use the long data type in order to
+               // avoid the insertion of "sext" instructions which
+               // could break the calculation for negative dependencies
   }
   return 0;
 }
